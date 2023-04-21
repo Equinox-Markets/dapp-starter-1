a38aaabbc756e61cc51ccd78ca137be69bb7c5c6
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 01:31:22 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -52,30 +52,29 @@ const Dashboard: FC = () => {
         </div>
       </div>
       
-   <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 mt-6 flex flex-col md:flex-row items-center justify-between">
-   {/* How to Get Rewards */}
-   <div className="text-center md:text-left">
-     <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">How to get rewards with Fantom Treasures</h2>
-     <ol className="list-decimal list-inside text-gray-500 dark:text-gray-400">
-       <li>
-         <h3 className="font-semibold">Mint NFT</h3>
-         <p>Convince the user here why they should get an NFT</p>
-         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-          Mint now
-         </button>
-       </li>
-       <li>
-         <h3 className="font-semibold">Stake</h3>
-         <p>Stake your NFT to earn rewards from Community Treasury on Fantom.</p>
-       </li>
-       <li>
-         <h3 className="font-semibold">Earn</h3>
-         <p>Earn rewards in $FTM.</p>
-       </li>
-       </ol>
-      </div>
-     </div>
+    <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 mt-6 flex flex-col md:flex-row items-center justify-between">
+    {/* How to Get Rewards */}
+    <div className="md:w-1/2 text-center md:text-left">
+      <h2 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-4">How to get rewards with Fantom Treasures</h2>
+      <ol className="list-decimal list-inside text-lg text-gray-700 dark:text-gray-300">
+        <li className="mb-4">
+          <h3 className="font-bold text-xl mb-2">Mint NFT</h3>
+          <p className="text-gray-500 dark:text-gray-400">Convince the user here why they should get an NFT</p>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-2">
+            Mint now
+          </button>
+        </li>
+        <li className="mb-4">
+          <h3 className="font-bold text-xl mb-2">Stake</h3>
+          <p className="text-gray-500 dark:text-gray-400">Stake your NFT to earn rewards from Community Treasury on Fantom.</p>
+        </li>
+        <li className="mb-4">
+          <h3 className="font-bold text-xl mb-2">Earn</h3>
+          <p className="text-gray-500 dark:text-gray-400">Earn rewards in $FTM.</p>
+        </li>
+      </ol>
     </div>
+  </div>
   )
 }
 
