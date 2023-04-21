880b6113fa404bd48e6f74a7ec2fc2f2b3f9da6f
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 02:09:22 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,20 +6,20 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-auto flex items-center justify-center md:justify-start mb-4 md:mb-0">
-            <img src="/images/logo-white.png" alt="Logo" className="h-8" />
+            <img src="/images/robot.png" alt="Logo" className="h-8" />
           </div>
           <div className="w-full md:w-auto flex items-center justify-center md:justify-end">
             <a href="https://twitter.com/" target="_blank" rel="noreferrer" className="mr-4">
-              <img src="/images/twitter.png" alt="Twitter" className="h-6" />
+              <img src="/images/robot.png" alt="Twitter" className="h-6" />
             </a>
             <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="mr-4">
-              <img src="/images/facebook.png" alt="Facebook" className="h-6" />
+              <img src="/images/robot.png" alt="Facebook" className="h-6" />
             </a>
             <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className="mr-4">
-              <img src="/images/instagram.png" alt="Instagram" className="h-6" />
+              <img src="/images/robot.png" alt="Instagram" className="h-6" />
             </a>
             <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer" className="mr-4">
-              <img src="/images/linkedin.png" alt="LinkedIn" className="h-6" />
+              <img src="/images/robot.png" alt="LinkedIn" className="h-6" />
             </a>
           </div>
         <p className="text-center mt-4">
