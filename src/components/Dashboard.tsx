33fb14d995e474f59d9de2c76ca8c110d4ca572a
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 00:33:23 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -25,16 +25,13 @@ const Dashboard: FC = () => {
             </div>
           </div>
           
-          {/* Add the ThemeSwitcher component */}
-          <div className="mt-4">
-            <ThemeSwitcher />
-          </div>
-        </div>
 
         {/* NFT Section */}
         <div className="mt-12 grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+                <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
           <div className="relative">
-            <Image src="/public/images/robot.png" alt="Fantom Treasures NFT" width={500} height={500} className="rounded-lg shadow-lg" />
+            <Image src="public/images/robot.png" alt="Fantom Treasures NFT" width={500} height={500} className="rounded-lg shadow-lg" />
           </div>
           <div>
             <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">Fantom Treasures</h2>
@@ -46,12 +43,16 @@ const Dashboard: FC = () => {
 
         {/* Earn real profit section */}
         <div className="mt-12">
+                <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
           <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">Earn real profit with Fantom Treasures</h2>
           <p className="mt-4 text-gray-700 dark:text-gray-300">Earn $WFTM from the platform&apos;s treasury with your NFT</p>
         </div>
 
         {/* Stats section */}
         <div className="mt-12 grid grid-cols-1 sm:grid-cols-3 gap-8">
+                <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
           <div className="p-6 border-2 border-gray-200 dark:border-gray-700 rounded-lg">
             <h3 className="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">Staked NFTs</h3>
             <p className="text-gray-500 dark:text-gray-400">1234</p>
@@ -68,11 +69,15 @@ const Dashboard: FC = () => {
         
             {/* Claim rewards section */}
     <div className="mt-12">
+            <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
       <button className="bg-indigo-600 dark:bg-indigo-400 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-indigo-500 dark:hover:bg-indigo-300">Claim Rewards</button>
     </div>
 
     {/* User's NFTs section */}
     <div className="mt-12">
+            <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
       <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">Your Fantom Treasure NFTs</h2>
       <p className="mt-4 text-gray-700 dark:text-gray-300">You do not own any Fantom Treasure NFTs.</p>
       <a href="https://link-to-nft-purchase" className="text-indigo-600 dark:text-indigo-400 hover:text-indigo-500 dark:hover:text-indigo-300">Get Your Fantom Treasure NFTs</a>
@@ -80,6 +85,8 @@ const Dashboard: FC = () => {
 
     {/* How to get rewards section */}
     <div className="mt-12">
+            <div className="container mx-auto p-6">
+        <div className="shadow-md rounded-lg p-6 mt-6">
       <h2 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">How to get rewards with Fantom Treasures</h2>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
         <div className="flex flex-col items-center text-center">
