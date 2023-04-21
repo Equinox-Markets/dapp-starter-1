e6e7e7cb25104bb84da0f6552cfcbd76df663c3a
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 02:07:51 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,35 @@ import { FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-400 py-8">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <ul className="flex justify-center space-x-6">
-          <li>
-            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-              <FaTwitter className="w-6 h-6" />
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-between">
+          <div className="w-full md:w-auto flex items-center justify-center md:justify-start mb-4 md:mb-0">
+            <img src="/images/logo-white.png" alt="Logo" className="h-8" />
+          </div>
+          <div className="w-full md:w-auto flex items-center justify-center md:justify-end">
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer" className="mr-4">
+              <img src="/images/twitter.png" alt="Twitter" className="h-6" />
             </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-              <FaInstagram className="w-6 h-6" />
+            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="mr-4">
+              <img src="/images/facebook.png" alt="Facebook" className="h-6" />
             </a>
-          </li>
-          <li>
-            <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-              <FaGithub className="w-6 h-6" />
+            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer" className="mr-4">
+              <img src="/images/instagram.png" alt="Instagram" className="h-6" />
             </a>
-          </li>
-        </ul>
+            <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer" className="mr-4">
+              <img src="/images/linkedin.png" alt="LinkedIn" className="h-6" />
+            </a>
+          </div>
         <p className="text-center mt-4">
           © {new Date().getFullYear()} My Website. All rights reserved.
         </p>
       </div>
+     </div>
     </footer>
   );
 };
