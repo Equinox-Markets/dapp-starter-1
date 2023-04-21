be58da4caf04579f3b53342c1fd0e745a573ecf4
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 03:06:25 -0500

    Update ConnectWallet.tsx

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -4,12 +4,28 @@ import { ConnectKitButton } from 'connectkit'
 
 type Visibility = 'always' | 'connected' | 'not_connected'
 
-const ConnectWallet: FC<{ show?: Visibility }> = ({ show = 'always' }) => {
-	const { isConnected } = useAccount()
-
-	if ((show == 'connected' && !isConnected) || (show == 'not_connected' && isConnected)) return null
-
-	return <ConnectKitButton />
+interface ConnectWalletProps {
+  show?: Visibility;
+  onClick?: () => void;
 }
 
-export default ConnectWallet
+const ConnectWallet: FC<ConnectWalletProps> = ({ show = 'always', onClick }) => {
+  const { isConnected } = useAccount()
+
+  if ((show == 'connected' && !isConnected) || (show == 'not_connected' && isConnected)) return null
+
+  return (
+    <ConnectKitButton
+      onClick={(e) => {
+        // Check if onClick prop is provided before calling it
+        if (onClick) {
+          e.preventDefault();
+          onClick();
+        }
+      }}
+    />
+  );
+};
+
+export default ConnectWallet;
+
