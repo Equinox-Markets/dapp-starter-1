2004ed301f6314b8e23520d59e40f5cc04a9eaeb
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 01:02:12 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -74,30 +74,38 @@ const Dashboard: FC = () => {
 
   </div>
 
-  {/* How to Get Rewards */}
-  <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
-    <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">
+{/* How to Get Rewards */}
+<div className="flex justify-center bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
+  <div className="max-w-2xl">
+    <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100 text-center">
       How to get rewards with Fantom Treasures
     </h2>
     <ol className="list-decimal list-inside text-gray-500 dark:text-gray-400">
-      <li>
-        <h3 className="font-semibold">Mint NFT</h3>
-        <p>Convince the user here why they should get an NFT</p>
+      <li className="mb-6">
+        <h3 className="font-semibold mb-2 text-gray-900 dark:text-gray-100">Mint NFT</h3>
+        <p className="mb-2">
+          Convince the user here why they should get an NFT
+        </p>
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
           Mint now
         </button>
       </li>
-      <li>
-        <h3 className="font-semibold">Stake</h3>
-        <p>Stake your NFT to earn rewards from Community Treasury on Fantom.</p>
+      <li className="mb-6">
+        <h3 className="font-semibold mb-2 text-gray-900 dark:text-gray-100">Stake</h3>
+        <p className="mb-2">
+          Stake your NFT to earn rewards from Community Treasury on Fantom.
+        </p>
       </li>
       <li>
-        <h3 className="font-semibold">Earn</h3>
-        <p>Earn rewards in $FTM.</p>
+        <h3 className="font-semibold mb-2 text-gray-900 dark:text-gray-100">Earn</h3>
+        <p>
+          Earn rewards in $FTM.
+        </p>
       </li>
     </ol>
   </div>
 </div>
+
 )
 }
 
