55ab9e87dee092161d3f3f0e55e4ba8bb2a9eec4
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 02:04:25 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import ConnectWallet from '@/components/ConnectWallet'
 import { BookOpenIcon, CodeIcon, ShareIcon } from '@heroicons/react/outline'
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 import Dashboard from '@/components/Dashboard'
+import Footer from "../components/Footer";
 
 const Home: FC = () => {
   return (
     <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
       <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <Dashboard />
+        <Footer />
 
         <div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
           {/* ... other elements */}
