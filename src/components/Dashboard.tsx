d11c5869b14fba06cc54822a2447917c33e8e016
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 02:44:14 -0500

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,66 @@
-import { FC } from 'react'
-import Image from 'next/image'
+import { FC, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { useAccount, useBalance } from 'wagmi'
-import { APP_NAME } from '@/lib/consts'
-import ConnectWallet from '@/components/ConnectWallet'
-import ThemeSwitcher from '@/components/ThemeSwitcher'
+import { useAccount, useBalance } from 'wagmi';
+import { APP_NAME } from '@/lib/consts';
+import ConnectWallet from '@/components/ConnectWallet';
+import ThemeSwitcher from '@/components/ThemeSwitcher';
+
+// Import the ethers library
+import { ethers } from 'ethers';
+
+// Replace with your smart contract ABI and address
+const contractAbi = [];
+const contractAddress = '';
 
 const Dashboard: FC = () => {
-  const { address } = useAccount()
-  const { data: balanceData } = useBalance({ token: 'ETH' })
+  const { address, provider } = useAccount();
+  const { data: balanceData } = useBalance({ token: 'ETH' });
 
-  const balance = balanceData?.formatted || '0'
+  const balance = balanceData?.formatted || '0';
 
+  const [userNFTs, setUserNFTs] = useState([]);
+  const [noNFTMessage, setNoNFTMessage] = useState('');
+
+  // Connect to the smart contract
+  const connectToContract = async () => {
+    if (!provider) return;
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+    return contract;
+  };
+
+  // Read user's wallet for the specific smart contract
+  const handleConnectWallet = async () => {
+    const contract = await connectToContract();
+    if (!contract || !address) return;
+
+    // Call the desired function in the smart contract to get the user's NFTs
+    // Replace 'getUserNFTs' with the name of the function you want to call
+    const nfts = await contract.getUserNFTs(address);
+
+    if (nfts.length === 0) {
+      setNoNFTMessage(
+        'You do not own any NFTs. You can purchase them here: '
+      );
+    } else {
+      setUserNFTs(nfts);
+    }
+  };
+
+  // Execute a specific function in the smart contract for Claim Rewards
+  const handleClaimRewards = async () => {
+    const contract = await connectToContract();
+    if (!contract) return;
+
+    // Replace 'claimRewards' with the name of the function you want to call
+    const tx = await contract.claimRewards();
+    await tx.wait();
+  };
+  
   return (
     <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
       {/* NFT Showcase */}
@@ -35,33 +84,58 @@ const Dashboard: FC = () => {
         <ThemeSwitcher className="absolute top-6 right-6" />
       </div>
 
-      {/* My NFTs */}
-      <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
-        <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">My NFTs</h2>
-        <div className="flex justify-center">
-          <p className="text-gray-500 dark:text-gray-400">{address ? 'Your NFTs will be displayed here' : 'Please connect your wallet to view your NFTs'}</p>
-        </div>
-        {!address && (
-          <div className="flex justify-center mt-4">
-            <ConnectWallet />
-          </div>
+     {/* My NFTs */}
+    <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
+      <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">My NFTs</h2>
+      <div className="flex justify-center">
+        {address ? (
+          userNFTs.length > 0 ? (
+            userNFTs.map((nft, index) => (
+              // Replace with the JSX structure you want to display for each NFT
+              <div key={index}>NFT {index + 1}: {nft}</div>
+            ))
+          ) : (
+            <p className="text-gray-500 dark:text-gray-400">
+              {noNFTMessage}
+              <a
+                href="https://example.com" // Replace with the actual website URL
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500 hover:text-blue-600"
+              >
+                Purchase NFTs
+              </a>
+            </p>
+          )
+        ) : (
+          <p className="text-gray-500 dark:text-gray-400">
+            Please connect your wallet to view your NFTs
+          </p>
         )}
-      </div>
-
-      {/* Claim Rewards */}
-      <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
-        <div className="text-center">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Claim Rewards</h2>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-            Claim Rewards
-          </button>
         </div>
+      {!address && (
+        <div className="flex justify-center mt-4">
+          <ConnectWallet onClick={handleConnectWallet} />
+        </div>
+      )}
+    </div>
+
+     {/* Claim Rewards */}
+    <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-6 mt-6">
+      <div className="text-center">
+        <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Claim Rewards</h2>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClaimRewards}
+        >
+          Claim Rewards
+        </button>
       </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Dashboard
+export default Dashboard;
 
 
 
