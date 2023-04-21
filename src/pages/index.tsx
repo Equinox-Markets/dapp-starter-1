13c434387908aeb073fa3b934a3b4038c761ca61
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-21 00:31:00 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,15 +9,11 @@ import Dashboard from '@/components/Dashboard'
 const Home: FC = () => {
   return (
     <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-      <div className="absolute top-6 right-6">
+      <div className="absolute top-6 right-6 flex">
+        <ThemeSwitcher className="mr-2" />
         <ConnectWallet />
       </div>
-      <ThemeSwitcher className="absolute bottom-6 right-6" />
       <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
-        <div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
-          <h1 className="text-6xl font-bold dark:text-white">{APP_NAME}</h1>
-        </div>
-
         <Dashboard />
 
         <div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
@@ -33,4 +29,5 @@ const Home: FC = () => {
 }
 
 export default Home
+
 
